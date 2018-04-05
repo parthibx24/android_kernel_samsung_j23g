68f5f0d1bdcff92cc644182bd94374e7d38dadc0
Parthib24 <sirajammunirparthi24@gmail.com>
2018-04-05 05:53:44 +0000

    Revert "hotplug: Make register and unregister notifier API symmetric"
    This reverts commit ce89669fce50e65ed86f4572782010fcb0a77d0e.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -195,6 +195,8 @@ static int cpu_notify(unsigned long val, void *v)
 	return __cpu_notify(val, v, -1, NULL);
 }
 
+#ifdef CONFIG_HOTPLUG_CPU
+
 static void cpu_notify_nofail(unsigned long val, void *v)
 {
 	BUG_ON(cpu_notify(val, v));
@@ -210,7 +212,6 @@ void __ref unregister_cpu_notifier(struct notifier_block *nb)
 }
 EXPORT_SYMBOL(unregister_cpu_notifier);
 
-#ifdef CONFIG_HOTPLUG_CPU
 void __ref __unregister_cpu_notifier(struct notifier_block *nb)
 {
 	raw_notifier_chain_unregister(&cpu_chain, nb);
